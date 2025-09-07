e1c695997a2ebe8968f975cd6dfc8b9cdcc9f00c
vishmitha <vishuvishmitha84@gmail.com>
2025-09-07 23:18:48 +0530

    Fix CORS to allow frontend requests

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -15,8 +15,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // your React app
-    credentials: true // 👈 this allows cookies to be sent
+  origin: "https://url-shortener-frontend-3ly1.onrender.com",
+  credentials: true
 }));
 
 app.use(express.json())
