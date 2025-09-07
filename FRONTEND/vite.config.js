4b6ec7ac5525932abe3475db8952bb8e261913bc
vishmitha <vishuvishmitha84@gmail.com>
2025-09-07 23:07:37 +0530

    Add allowedHosts for Render deploy

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: [
+      'url-shortener-frontend-3ly1.onrender.com' // your Render frontend URL
+    ],
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0'
+  }
 })
